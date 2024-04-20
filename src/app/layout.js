b4238880bcbb92bfc267433a3155e60141fd2065
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-04-20 22:43:01 +0600

    start a check code

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { getServerSession } from "next-auth";
 import Navbar from "./../components/Navbar"
+import Footer from "./../components/Footer"
+import AuthProvider from "./../utils/AuthProvider"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,12 +19,12 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <AuthProvider session={session}> */}
+        <AuthProvider session={session}>
           {/* Start Navbar */}
           <Navbar />
           {children}
-        {/* </AuthProvider> */}
-      
+        </AuthProvider>
+      <Footer />
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
 
                         <div class="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                             <div class="max-w-xl mx-auto text-center lg:max-w-md xl:max-w-lg lg:text-left lg:mx-0">
-                                <h1 class="text-3xl font-bold text-white sm:text-4xl xl:text-5xl xl:leading-tight">Build SaaS Landing Page without Writing a Single Code</h1>
+                                <h1 class="text-3xl font-bold text-white sm:text-4xl xl:text-5xl xl:leading-tight">Visit the store to buy produce directly from the farmer</h1>
                                 <p class="mt-8 text-base font-normal leading-7 text-gray-400 lg:max-w-md xl:pr-0 lg:pr-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nunc nisl eu consectetur. Mi massa elementum odio eu viverra amet.</p>
 
                                 <div class="flex items-center justify-center mt-8 space-x-5 xl:mt-16 lg:justify-start">
